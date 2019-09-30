ebdaf79271919405ed8f16513acd2e7bb6bdd2f1
npmyers <55990027+npmyers@users.noreply.github.com>
2019-09-30 15:42:06 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input  avalue of t:");
+
+            double t = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("input value of x :");
+
+            double x = Double.Parse(Console.ReadLine());
+
+            double y;
+
+            Console.WriteLine("input value for n:");
+
+            double n = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("input value for m:");
+
+            double m = Int32.Parse(Console.ReadLine());
+
+
+            y = Math.Exp(-t) * ((Math.Sin((n * x * Math.PI) / 2)) + (Math.Sin((m * x * Math.PI) / 2)));
+
+            Console.WriteLine(y);
+
+
+
+
+
+        }
+    }
+}
